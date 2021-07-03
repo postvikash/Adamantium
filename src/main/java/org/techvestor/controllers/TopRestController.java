49a7cdc51f5ff55c3cf44051e401e9d8d995f7a1
kmarnox <kmarnox@amazon.com>
2021-07-03 21:02:23 +0530

    Added news container with bootstrap grid and column

--- src/main/java/org/techvestor/controllers/TopRestController.java
+++ src/main/java/org/techvestor/controllers/TopRestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.techvestor.models.TopHeadlines;
@@ -16,10 +17,10 @@ public class TopRestController {
 
     @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping(value="/top-headlines", method =  RequestMethod.GET)
-    public TopHeadlines topHeadlines() {
+    public TopHeadlines topHeadlines(@RequestParam String newsTopic) {
         Gson gson = new Gson();
         String response = restTemplate
-                .getForObject("https://gnews.io/api/v4/top-headlines?token=a0a30dd5d88ce253266642a7ee0c0fe2&lang=en&country=in", String.class);
+                .getForObject("https://gnews.io/api/v4/top-headlines?token=a0a30dd5d88ce253266642a7ee0c0fe2&lang=en&country=in&topic="+newsTopic, String.class);
         TopHeadlines headlines = gson.fromJson(response, TopHeadlines.class);
         System.out.println(response);
         return headlines;
